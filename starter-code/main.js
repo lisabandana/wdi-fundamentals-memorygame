a3d20c359463ab5c977d82211fa15315155d4777
Sara Cheng <sarazwh@Saras-MacBook-Air.local>
2017-01-08 23:38:28 +0800

    added alert function

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,34 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-cardOne ==cardFour
+var cardOne = "Queen";
+var cardTwo = "Queen";
+var cardThree = "King";
+var cardFour = "King";
+if (cardTwo === cardFour){
+	alert ("try again");
+}
+else if (cardTwo === cardThree){
+	alert ("try again");
+}
+else {
+	alert ("found a match!");
+}
+if (cardOne === cardThree){
+	alert ("try again");
+}
+else if (cardOne === cardFour){
+	alert ("try again");
+}
+else {
+	alert ("found a match!");
+}
+if (cardThree === cardFour){
+	alert ("found a match");
+}
+else {
+	alert ("try again");
+}
+if (cardFour === cardThree){
+	alert ("found a match")
+}
+else {
+	alert ("try again");
+}
