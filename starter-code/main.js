c1d9f107f8afd115a52e9d76fc29f22a3b9937ba
Sara Cheng <sarazwh@Saras-MacBook-Air.local>
2017-01-09 15:20:43 +0800

    added functions

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,40 +1,39 @@
 var cardOne = "Queen";
 var cardTwo = "Queen";
 var cardThree = "King";
-var cardFour = "King";
+var cardFour = "Queen";
 
-if (cardTwo === cardFour){
-	alert ("try again");
-}
-else if (cardTwo === cardThree){
-	alert ("try again");
-}
-else if (cardOne === cardThree){
-	alert ("try again");
-}
-else if (cardOne === cardFour){
-	alert ("try again");
-}
-else if (cardTwo === cardOne){
-	alert ("found a match!");
-}
-else if (cardThree === cardFour){
-	alert ("found a match");
-}
-else {
-	alert ("try again");
-}
+// if (cardTwo === cardFour){
+// 	alert ("try again");
+// } else if (cardTwo === cardThree){
+// 	alert ("try again");
+// } else if (cardOne === cardThree){
+// 	alert ("try again");
+// } else if (cardOne === cardFour){
+// 	alert ("try again");
+// } else if (cardTwo === cardOne){
+// 	alert ("found a match!");
+// } else if (cardThree === cardFour){
+// 	alert ("found a match");
+// } else {
+// 	alert ("try again");
+// }
+
 
 var cardNumber = document.getElementById('game-board');
-for (var i = 0; i < cardNumber.length; i++){
-	cardNumber[i].className = 'cardOne';
+
+var createCards = function(maxCards) {
+	for (var i = 0; i < maxCards; i++){
+		var div = document.createElement('div');
+		div.className = 'card';
+		cardNumber.appendChild(div);
+	}
+
 }
-for (var i = 0; i < cardNumber.length; i++){
-	cardNumber[i].className = 'cardTwo';
-}
-for (var i = 0; i < cardNumber.length; i++){
-	cardNumber[i].className = 'cardThree';
-}
-for (var i = 0; i < cardNumber.length; i++){
-	cardNumber[i].className = 'cardFour';
-}
+
+createCards(4);
+
+
+
+
+
